90e3c4ece4e3924fd4ffe991b0963a82966214d7
César Arana <carana@espol.edu.ec>
2024-07-17 17:41:07 -0500

    Actualizando proyecto

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,10 @@ var router = express.Router();
  var initModels = require("../models/init-models");
  var models = initModels( sequelize );  
 
- /* GET users listing. */
+  /* 1. Importe el módulo crypto */
+  let crypto = require('crypto');
+
+  /* GET users listing. */
  /* 2. Convierta el callback en asíncrono */
  router.get('/', async function(req, res, next) {
 
@@ -17,5 +20,31 @@ var router = express.Router();
    res.render('crud', { title: 'CRUD with users', usersArray: usersCollection });
 
  });
+
+ router.post('/', async (req, res) => {
+
+  /* 3. Desestructure los elementos en el cuerpo del requerimiento */
+  let { name, password, idrole } = req.body;
+
+  try {
+
+    /* 4. Utilice la variable SALT para encriptar la variable password. */
+    let salt = process.env.SALT
+    let hash = crypto.createHmac('sha512', salt).update(password).digest("base64");
+    let passwordHash = salt + "$" + hash
+
+    /* 5. Guarde el registro mediante el método create */
+    let user = await models.users.create({ name: name, password: passwordHash })
+
+    /* 6. Redireccione a la ruta con la vista principal '/users' */
+    res.redirect('/users')
+
+  } catch (error) {
+
+    res.status(400).send(error)
+
+  }
+
+})
 
 module.exports = router;

0629353cc7576b613d1957c1462ca0ec14ce5e9d
César Arana <carana@espol.edu.ec>
2024-07-17 18:47:10 -0500

    Actualizando proyecto

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
- /* 1. Cargue los modelos de acuerdo con la configuración de la conexión */
- const sequelize = require('../models/index.js').sequelize;
- var initModels = require("../models/init-models");
- var models = initModels( sequelize );  
+/* 1. Cargue los modelos de acuerdo con la configuración de la conexión */
+const sequelize = require('../models/index.js').sequelize;
+var initModels = require("../models/init-models");
+var models = initModels(sequelize);
 
-  /* 1. Importe el módulo crypto */
-  let crypto = require('crypto');
+/* 1. Importe el módulo crypto */
+let crypto = require('crypto');
 
-  /* GET users listing. */
- /* 2. Convierta el callback en asíncrono */
- router.get('/', async function(req, res, next) {
+/* GET users listing. */
+/* 2. Convierta el callback en asíncrono */
+router.get('/', async function (req, res, next) {
 
-   /* 3. Uso del método findAll */
-   let usersCollection = await models.users.findAll({ })
+  /* 3. Uso del método findAll */
+  let usersCollection = await models.users.findAll({})
+  let rolesCollection = await models.roles.findAll({ })
+  /* 4. Paso de parámetros a la vista */
+  res.render('crud', { title: 'CRUD of users', usersArray: usersCollection, rolesArray: rolesCollection   });
 
-   /* 4. Paso de parámetros a la vista */
-   res.render('crud', { title: 'CRUD with users', usersArray: usersCollection });
+});
 
- });
 
- router.post('/', async (req, res) => {
+
+router.post('/', async (req, res) => {
 
   /* 3. Desestructure los elementos en el cuerpo del requerimiento */
   let { name, password, idrole } = req.body;
@@ -33,9 +35,11 @@ var router = express.Router();
     let hash = crypto.createHmac('sha512', salt).update(password).digest("base64");
     let passwordHash = salt + "$" + hash
 
+
+
     /* 5. Guarde el registro mediante el método create */
     let user = await models.users.create({ name: name, password: passwordHash })
-
+    await models.users_roles.create({ users_iduser: user.iduser, roles_idrole: idrole })
     /* 6. Redireccione a la ruta con la vista principal '/users' */
     res.redirect('/users')
 
@@ -46,5 +50,24 @@ var router = express.Router();
   }
 
 })
+
+/* DELETE user */
+router.delete('/:iduser', async (req, res) => {
+  let iduser = req.params.iduser;
+
+  try {
+    /* 1. Eliminar las relaciones en user_roles */
+    await models.user_roles.destroy({ where: { iduser: iduser } });
+
+    /* 2. Eliminar el usuario */
+    await models.users.destroy({ where: { iduser: iduser } });
+
+    /* 3. Redireccione a la ruta con la vista principal '/users' */
+    res.redirect('/users');
+
+  } catch (error) {
+    res.status(400).send(error)
+  }
+});
 
 module.exports = router;
